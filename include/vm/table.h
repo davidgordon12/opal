2b888d1d50307034a2eb4eff46e0aef1ed6a776d
David Gordon <davidgordon12@live.ca>
2023-12-11 15:02:25 -0500

    Create table.h

--- include/vm/table.h
+++ include/vm/table.h
@@ -2,5 +2,19 @@
 #define _TABLE_H
 
 #include "common.h"
+#include "vm/values.h"
+
+typedef struct {
+    object_string* key;
+    value val;
+} entry;
+
+typedef struct {
+    uint64_t count;
+    uint64_t capacity;
+    entry* entries;
+} table;
+
+void init_table(table* tbl);
 
 #endif
