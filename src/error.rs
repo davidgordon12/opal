564a7dbf4907bdfa8fcbadc8ba6e241aafb4cd7c
tehtko <moderndsnsam@gmail.com>
2023-12-26 17:17:59 -0500

    Update error codes to exit silently (we have our own warnings)

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,17 @@
 pub fn error(message: &str, error: Option<&str>) {
     println!("Opal: \x1b[91mFatal Error\x1b[0m");
     println!("{} \x1b[93m{}\x1b[0m", message, error.unwrap_or(""));
-    std::process::exit(1);
+    std::process::exit(0);
 }
 
 pub fn operation_error(message: &str, line: &str) {
     println!("Opal: \x1b[91mFatal Error\x1b[0m");
     println!("{} Line \x1b[93m{}\x1b[0m", message, line);
-    std::process::exit(1);
+    std::process::exit(0);
 }
 
 pub fn parse_token_error(message: &str, token_literal: &str, line: &str) {
     println!("Opal: \x1b[91mFatal Error\x1b[0m");
     println!("{} \x1b[93m{}\x1b[0m on line \x1b[93m{}\x1b[0m", message, token_literal, line);
-    std::process::exit(1);
+    std::process::exit(0);
 }
