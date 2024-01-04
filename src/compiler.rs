55ffd4ba560d894d26a2cc422c466765d549e799
tehtko <moderndsnsam@gmail.com>
2024-01-04 17:56:08 -0500

    Optimize operations

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,6 +1,4 @@
 use std::io::Write;
-use std::os::unix::process::CommandExt;
-use std::process::Command;
 
 use crate::ast::{Program, Expr, BinaryExpr};
 use crate::error::error;
@@ -21,12 +19,11 @@ impl Compiler {
     }
 
     pub fn create_asm(&self) {
-        let mut file = std::fs::File::create(&self.file_path).unwrap();
-        file.write(b"section .text
-global _start
-
-_start:"
-        ).unwrap();
+        let _ = std::fs::remove_file(&self.file_path);
+        let mut file = std::fs::File::options().append(true).create(true).open(&self.file_path).unwrap();
+        file.write(b"section .text\n").unwrap();
+        file.write(b"global _start\n").unwrap();
+        file.write(b"\n_start:").unwrap();
     }
 
     pub fn run(&mut self) {
@@ -34,8 +31,12 @@ _start:"
         self.create_asm();
 
         /* Walk the tree here and determine the compilation method for the specific node */
-        for x in &self.program.body {
-            self.compile_binary_expr(x.clone().unwrap_binary_expr());
+        for expr in &self.program.body {
+            match expr {
+                Expr::BinaryExpr(x) => self.compile_binary_expr(x.clone()),
+                _ => {},
+            }
+            
         }
         self.exit();
     }
@@ -73,15 +74,13 @@ _start:"
 
         let mut arg: String = String::from("mov rax, ");
         arg.push_str(&a.to_string());
+
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
 
-        let mut arg: String = String::from("mov rbx, ");
+        let mut arg: String = String::from("add rax, ");
         arg.push_str(&b.to_string());
-        file.write(b"\n        ").unwrap();
-        file.write(arg.as_bytes()).unwrap();
 
-        let arg: String = String::from("add rax, rbx");
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
     }
@@ -91,15 +90,13 @@ _start:"
 
         let mut arg: String = String::from("mov rax, ");
         arg.push_str(&a.to_string());
+
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
 
-        let mut arg: String = String::from("mov rbx, ");
+        let mut arg: String = String::from("sub rax, ");
         arg.push_str(&b.to_string());
-        file.write(b"\n        ").unwrap();
-        file.write(arg.as_bytes()).unwrap();
 
-        let arg: String = String::from("sub rax, rbx");
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
     }
@@ -109,15 +106,13 @@ _start:"
 
         let mut arg: String = String::from("mov rax, ");
         arg.push_str(&a.to_string());
+
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
 
-        let mut arg: String = String::from("mov rbx, ");
+        let mut arg: String = String::from("mul rax, ");
         arg.push_str(&b.to_string());
-        file.write(b"\n        ").unwrap();
-        file.write(arg.as_bytes()).unwrap();
 
-        let arg: String = String::from("mul rbx");
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
     }
@@ -127,15 +122,13 @@ _start:"
 
         let mut arg: String = String::from("mov rax, ");
         arg.push_str(&a.to_string());
+
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
 
-        let mut arg: String = String::from("mov rbx, ");
+        let mut arg: String = String::from("div rax, ");
         arg.push_str(&b.to_string());
-        file.write(b"\n        ").unwrap();
-        file.write(arg.as_bytes()).unwrap();
 
-        let arg: String = String::from("div rbx");
         file.write(b"\n        ").unwrap();
         file.write(arg.as_bytes()).unwrap();
     }
